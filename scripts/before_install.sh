49c9e6596c5b087d07e54643ce29297b65da09c8
Mandeep <mandeep_singh9398@yahoo.in>
2021-12-02 12:20:09 +0530

    Modified scripts

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+sudo yum update
+
 # Install node.js
-sudo apt-get install python-software-properties -y
-sudo apt-add-repository ppa:chris-lea/node.js -y
-sudo apt-get update
-sudo apt-get install nodejs -y
+sudo yum install python-software-properties -y
+sudo yum install ppa:chris-lea/node.js -y
+sudo yum update
+sudo yum install nodejs -y
 
 # Install nodemon
 # sudo npm install nodemon -g
